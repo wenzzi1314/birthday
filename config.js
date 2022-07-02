ba4657bddb64648706515d01c4b54994544c8b4f
wenzzi1314 <75979277+wenzzi1314@users.noreply.github.com>
2022-07-02 22:24:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,40 +1,29 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
-    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
-    // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "最可爱的小美玉",  // 同上...
+        "送给",      
+        "最可爱的小美玉",  
         "今天是你的生日",
         "这是陪你过的",
         "第一个生日",
         "去年的生日",
         "是和你朋友一起过的哦",
-        "今年加上我哦",
+        "今年加上我啦",
         "要好好吃饭，早早睡觉",
+        "最重要的是，天天开心！",
+        "For the best of you",
     ],
-    /**
-     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
-     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
-     * 例如
-     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
-     *
-     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
-     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
-     */
     imgs: {
         "最可爱的小美玉": "./imgs/meiyu.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        //"For the best of you": "./imgs/birthday.jpg",
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        play: "少点声音？",
+        bannar_coming: "颜色好像有些单调",
+        balloons_flying: "好像还是少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "还有，生日快乐！",
         story: "A MESSAGE FOR YOU",
     }
 };
